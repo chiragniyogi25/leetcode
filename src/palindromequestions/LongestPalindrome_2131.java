6a916bde25d60474a3040b900373bce80ef54e40
chiragniyogi25 <niyogichirag@gmail.com>
2022-11-13 21:32:17 +0530

    LongestPalindrome_2131

--- src/palindromequestions/LongestPalindrome_2131.java
+++ src/palindromequestions/LongestPalindrome_2131.java
@@ -8,6 +8,8 @@ import java.util.Map;
 public class LongestPalindrome_2131 {
     //Time Complexity->O(n)+O(m)
     //space Complexity->O(m)->since hashmap is been used
+
+    //Using HashMap
     public static int longestPalindrome(String[] words){
         int len=0;
         Map<String,Integer> map=new HashMap<>();
@@ -52,12 +54,35 @@ public class LongestPalindrome_2131 {
     public static String reverse(String str){
         return ""+str.charAt(1)+str.charAt(0);
     }
+    //Without HashMap
+    //space-Complexity->O(26*26)
+    public static int longestPalindromeWithoutHashMap(String[] words){
+        int len=0;
+        int[][] freq=new int[26][26];
+        for(String str:words){
+            int index1=str.charAt(0)-'a';
+            int index2=str.charAt(1)-'a';
+            //if reverse found for the current string
+            if(freq[index2][index1]>0){
+                --freq[index2][index1];
+                len+=4;
+            }else
+                //if reverse not found
+                ++freq[index1][index2];
+        }
+        //case for gg,aa->can have only 1 at middle if present
+        for(int i=0;i<26;i++){
+            if(freq[i][i]>0)return len+2;
+        }
+        return len;
+    }
 
     public static void main(String[] args) {
 //        String[] words={"dd","aa","bb","dd","aa","dd","bb","dd","aa","cc","bb","cc","dd","cc"};
 //        String[] words={"ab","ty","yt","lc","cl","ab"};
         String[] words={"mm","mm","yb","by","bb","bm","ym","mb","yb","by","mb","mb","bb","yb","by","bb","yb","my","mb","ym"};
         System.out.println(longestPalindrome(words));
+        System.out.println(longestPalindromeWithoutHashMap(words));
     }
     /**
      * dd->5=>4*2=8
